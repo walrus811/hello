5f1dd083d98b4e84399afa7a52ff7c3c0b640360
ray <walrusray811@gmail.com>
2024-05-16 11:07:28 +0900

    init

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/walrus811/hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
